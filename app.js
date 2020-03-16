a9d909b219010d5dae4f189ff3e7928829da3500
wijaksanapanji <wijaksanapanji6@gmail.com>
2020-03-16 23:29:23 +0800

    init

---
+++ app.js
@@ -0,0 +1,26 @@
+let date = new Date("April 1, 2020").getTime();
+
+const countdown = setInterval(function() {
+  let now = new Date().getTime();
+  let distance = date - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.querySelector(".date").innerHTML =
+    days +
+    " Hari " +
+    hours +
+    " Jam " +
+    minutes +
+    " Menit " +
+    seconds +
+    " Detik";
+
+  if (distance < 0) {
+    clearInterval(countdown);
+    document.querySelector(".date").innerHTML = "10 April 2020";
+  }
+});
